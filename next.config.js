7e250d159e9ad5a7291f07c6c9a463411fb168ba
Joe Rebagliati <84640075+J-Rebs@users.noreply.github.com>
2023-01-04 18:52:39 -0500

    fixes

--- next.config.js
+++ next.config.js
@@ -7,10 +7,11 @@ const rehypeKatex = require('rehype-katex')
 const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.js',
-  unstable_staticImage: true,
+  // ...
   mdxOptions: {
     remarkPlugins: [remarkMath],
     rehypePlugins: [rehypeKatex],
   },
 })
+
 module.exports = withNextra()
